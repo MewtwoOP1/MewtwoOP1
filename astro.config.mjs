e6c6151adff55cf362e99bf75bcf811d8620ef7a
MewtwoOP1 <153761326+MewtwoOP1@users.noreply.github.com>
2026-09-10 17:28:22 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://mewtwoop1.github.io',
-  base: '/',
+  site: 'https://mewtwoop1.github.io/MewtwoOP1',
+  base: '/MewtwoOP1/',
   output: 'static'
 });
